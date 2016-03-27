24f3cfbbae4082d53e43499c1e8e4695b3df7f8a
Jeff Hollan <jeffhollan@outlook.com>
2016-03-26 20:45:05 -0700

    initial

---
+++ VSTS_API/Models/SimpleWorkItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VSTS_API.Models
+{
+    public class SimpleWorkItem
+    {
+        //Title = "Parking Lot Backlog",
+        //        AreaPath = @"RD\Azure App Plat\Logic Apps",
+        //        State = "New",
+        //        IterationPath = @"RD\Azure App Plat\Logic Apps"
+
+        public string Type { get; set; }
+        public string Title { get; set; }
+        public string AreaPath { get; set; }
+        public string State { get; set; }
+        public string IterationPath { get; set; }
+        public string LinkType { get; set; }
+        public int LinkId { get; set; }
+
+    }
+}
